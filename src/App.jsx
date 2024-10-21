a085ffd21c721b966ac9c021f68a790bbbf70f8e
gcarlab <125522911+gcarlab@users.noreply.github.com>
2024-10-20 22:31:54 -0500

    Add CssBaseline to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./Navbar"
-import { TextField, IconButton } from '@mui/material'
+import { TextField, IconButton, CssBaseline } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
 
 function App() {
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
       {/* Navigation bar implementation */}
+      <CssBaseline />
       <Navbar />
       <div id="search" style={{display: "flex", justifyContent: "center", padding: "5%"}}>
       <TextField label="Search" variant="standard" sx={{width: "50%"}}/>
