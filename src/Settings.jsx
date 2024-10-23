98a47ced2b488a2e8b81cb2df9fd25c96153e8d8
DrakeCal <drakeacalhoun@gmail.com>
2024-10-23 17:42:18 -0500

    Small settings dialog change
    Settings:
    - Dialog box is bigger
    - Dialog title changed a tad bit to be more centered
    - Dark mode setting has some dashes added to fit since it still sticks to the right

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -15,10 +15,10 @@ const Settings = () => {
         </IconButton>
 
         {/* Opens dialog box */}
-        <Dialog open={open} onClose={() => setOpen(false)} aria-labelledby="dialog-title">
+        <Dialog open={open} onClose={() => setOpen(false)} aria-labelledby="dialog-title" PaperProps={{sx: {width: "15rem", height: "10rem", justifyContent: "center"}}}>
 
             {/* Title of dialog box */}
-            <DialogTitle id="dialog-title" sx={{textAlign: "center", marginLeft: "-1rem"}}>
+            <DialogTitle id="dialog-title" sx={{textAlign: "center", marginLeft: "10px"}}>
                 Settings
                 {/* Close button */}
                 <IconButton onClick={() => setOpen(false)}>
@@ -29,7 +29,7 @@ const Settings = () => {
             <DialogActions>
                 <FormGroup>
                     {/* Dark mode switch */}
-                    <FormControlLabel control={<Switch />} label="Dark Mode"></FormControlLabel>
+                    <FormControlLabel control={<Switch />} label="- - - Dark Mode - - -"></FormControlLabel>
                 </FormGroup>
             </DialogActions>
         </Dialog>
