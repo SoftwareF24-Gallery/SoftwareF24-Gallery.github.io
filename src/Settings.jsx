b9084ca16eaeeb8cf879cc22b54a20acdde25422
The-Jragon <sethhill80@gmail.com>
2024-10-28 00:25:28 -0500

    enlarged settings page

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -15,7 +15,7 @@ const Settings = () => {
         </IconButton>
 
         {/* Opens dialog box */}
-        <Dialog open={open} onClose={() => setOpen(false)} aria-labelledby="dialog-title" PaperProps={{sx: {width: "15rem", height: "10rem", justifyContent: "center"}}}>
+        <Dialog open={open} onClose={() => setOpen(false)} aria-labelledby="dialog-title" PaperProps={{sx: {width: "100rem", height: "20rem", justifyContent: "center"}}}>
 
             {/* Title of dialog box */}
             <DialogTitle id="dialog-title" sx={{textAlign: "center", marginLeft: "10px"}}>
