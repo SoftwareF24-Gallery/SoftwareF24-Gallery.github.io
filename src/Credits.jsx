241c940e597b9ec0a3981f4c9530a77efb45c9b9
DrakeCal <drakeacalhoun@gmail.com>
2024-10-29 15:41:48 -0500

    Credits commit numero uno
    Made the credits look more organized with images. Committing this early because I want to do an overhaul with mui stuff.

--- src/Credits.jsx
+++ src/Credits.jsx
@@ -3,17 +3,30 @@ const Credits = () => {
         <>
 
         <div>
-            <h1>Credits</h1>
-            <ul>
-                <li>
-                    Me: Did cool stuff
+            <h1 style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>Credits</h1>
+
+            <ul style={{listStyleType: "none", fontSize: "100%", display: "flex", flexDirection: "column", gap: "5rem"}}>
+
+                <li style={{display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem" }}>
+                    <img src="JSONS\images\Steve headshot.jpeg" width="5%"></img>
+                    <p><b>This Guy</b>: Did cool stuff</p>
                 </li>
-                <li>
-                    Myself: Did cool stuff
+
+                <li style={{display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem"}}>
+                    <img src="JSONS\images\Steve headshot.jpeg" width="5%"></img>
+                    <p><b>This Guy</b>: Did cool stuff</p>
                 </li>
-                <li>
-                    I: Did cool stuff
+
+                <li style={{display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem" }}>
+                    <img src="JSONS\images\Steve headshot.jpeg" width="5%"></img>
+                    <p><b>This Guy</b>: Did cool stuff</p>
                 </li>
+
+                <li style={{display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem" }}>
+                    <img src="JSONS\images\Steve headshot.jpeg" width="5%"></img>
+                    <p><b>This Guy</b>: Did cool stuff</p>
+                </li>
+
             </ul>
         </div>
 
