83c36cebe2827b5af41a6e5a939b7b41976b9421
DrakeCal <drakeacalhoun@gmail.com>
2024-11-01 12:42:44 -0500

    Credits commit numero dos
    Same thing but I used MUI Grid instead of an unordered list.

--- src/Credits.jsx
+++ src/Credits.jsx
@@ -1,34 +1,30 @@
+import Grid from "@mui/material/Grid2"
+
 const Credits = () => {
     return (
         <>
+        <h1 style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>Credits</h1>
 
-        <div>
-            <h1 style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>Credits</h1>
-
-            <ul style={{listStyleType: "none", fontSize: "100%", display: "flex", flexDirection: "column", gap: "5rem"}}>
-
-                <li style={{display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem" }}>
-                    <img src="JSONS\images\Steve headshot.jpeg" width="5%"></img>
-                    <p><b>This Guy</b>: Did cool stuff</p>
-                </li>
-
-                <li style={{display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem"}}>
-                    <img src="JSONS\images\Steve headshot.jpeg" width="5%"></img>
-                    <p><b>This Guy</b>: Did cool stuff</p>
-                </li>
-
-                <li style={{display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem" }}>
-                    <img src="JSONS\images\Steve headshot.jpeg" width="5%"></img>
-                    <p><b>This Guy</b>: Did cool stuff</p>
-                </li>
-
-                <li style={{display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem" }}>
-                    <img src="JSONS\images\Steve headshot.jpeg" width="5%"></img>
-                    <p><b>This Guy</b>: Did cool stuff</p>
-                </li>
-
-            </ul>
-        </div>
+        <Grid container spacing={6}>
+            
+            <Grid size={10} sx={{display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem"}}>
+                <img src="JSONS\images\Steve headshot.jpeg" width="5%"></img>
+                <p><b>This Guy</b>: Did cool stuff</p>
+            </Grid>
+            <Grid size={10} sx={{display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem"}}>
+                <img src="JSONS\images\Steve headshot.jpeg" width="5%"></img>
+                <p><b>This Guy</b>: Did cool stuff</p>
+            </Grid>
+            <Grid size={10} sx={{display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem"}}>
+                <img src="JSONS\images\Steve headshot.jpeg" width="5%"></img>
+                <p><b>This Guy</b>: Did cool stuff</p>
+            </Grid>
+            <Grid size={10} sx={{display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem"}}>
+                <img src="JSONS\images\Steve headshot.jpeg" width="5%"></img>
+                <p><b>This Guy</b>: Did cool stuff</p>
+            </Grid>
+            
+        </Grid>
 
         </>
 
