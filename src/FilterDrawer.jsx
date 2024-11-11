bac612cbd39f2944b2d89ee9ab8bff6d6dbf854d
Michaelmadeit2001 <155524322+Michaelmadeit2001@users.noreply.github.com>
2024-11-10 21:58:33 -0600

    Added changes to drawer (in desc)
    added closing function that allows user to close drawer if they click of it and some text + hamburger menu within it that allows user to click it to close as well

--- src/FilterDrawer.jsx
+++ src/FilterDrawer.jsx
@@ -1,5 +1,7 @@
-import { Drawer, IconButton } from "@mui/material";
-import TuneIcon from '@mui/icons-material/Tune';
+import { Drawer, IconButton, Typography, ClickAwayListener } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box';
+import Tooltip from '@mui/material/Tooltip';
 import { useState } from "react";
 
 const drawerWidth = 240;
@@ -17,36 +19,74 @@ const FilterDrawer = () => {
         setOpen(false);
       };
 
-    return(
-        <>
+    return (
+        <div>
+         {/* Tooltip Adds alt text over hamburger menu, that says filters*/}
+          <Tooltip title="Filters">
             <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={[
-                {
-                mr: 2,
-                },
+              color="inherit"
+              aria-label="open drawer"
+              onClick={(event) => {
+                handleDrawerOpen();
+                 {/* Propagation helps prevent click from traveling to parent elements */}
+                event.stopPropagation();
+              }}
+              edge="start"
+              sx={[
+                { mr: 2 },
+                { mb: 2 },
                 open,
-            ]}
+              ]}
             >
-                <TuneIcon />
+              <MenuIcon />
             </IconButton>
+          </Tooltip>
+        
+        {/* Allows user to click off drawer and it will close*/}
+        <ClickAwayListener onClickAway={() => setOpen(false)}>
             <Drawer 
-            sx={{width: drawerWidth,
+                sx={{
+                width: drawerWidth,
                 flexShrink: 0,
                 '& .MuiDrawer-paper': {
                     width: drawerWidth,
                     boxSizing: 'border-box',
                 },
-            }}
-            variant="persistent"
-            anchor="left"
-            open={open}
+                }}
+                variant="persistent"
+                anchor="left"
+                open={open}
             >
+                <Box display={'flex'}>
+
+            {/* Filter Text within drawer*/}
+                    <Typography variant="h6"
+                    sx={{display: 'flex',
+                        mt: '2px',
+                        fontWeight: 500, 
+                        '&:hover' : {
+                            textDecoration: 'underline'
+                        }
+                    }}
+                    >Filters</Typography>
+            
+             {/* Hamburger menu within nav drawer*/}
+                <IconButton 
+                    sx={{
+                    marginLeft: '140px',
+                    '&:hover': { // Add a hover effect to the IconButton
+                        backgroundColor: 'rgba(0, 0, 0, 0.1)', // Slightly change background color on hover
+                    }
+                    }}
+                    onClick={handleDrawerClose}
+                >
+                    <MenuIcon /> 
+                </IconButton>
+                
+                </Box>
             </Drawer>
-        </>
+        </ClickAwayListener>     
+        </div>       
     )
 }
 
