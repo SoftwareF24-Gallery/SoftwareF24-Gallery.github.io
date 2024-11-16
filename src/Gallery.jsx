9c00dd564d7fbd89cba85974d6f95ec3bf74e48e
DrakeCal <drakeacalhoun@gmail.com>
2024-11-16 16:53:03 -0600

    Gallery Clicks
    - Click on image to show image. Unfortunately, it only shows the bike at the moment.

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -3,8 +3,13 @@ import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import artData from '../json/artData.json';
+import { Dialog, Typography, Card, CardMedia, CardContent } from '@mui/material'
+import { useState } from "react";
 
 const Gallery = () => {
+
+    const [open, setOpen] = useState(false)
+
     return(
         <div style={{alignItems: "flex-start", padding: "3%"}}>
           <FilterDrawer/>
@@ -12,7 +17,7 @@ const Gallery = () => {
               <ImageList variant="masonry" cols={4} gap={8}>
                 {/* Creating an array of img from objects in artData */}
                 {artData.map((item) => (
-                  <ImageListItem key={item.img}>
+                  <ImageListItem key={item.img} onClick={() => setOpen(true)}>
                       <img
                       srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
                       src={`${item.img}?w=248&fit=crop&auto=format`}
@@ -20,6 +25,26 @@ const Gallery = () => {
                       loading="lazy"
                       href="#"
                       />
+
+                  <Dialog open={open} onClose={() => setOpen(false)} sx={{background: 'rgba(0, 0, 0, 0.3)'}}>
+                    <Card sx={{ width: 450 }} elevation="10">
+                      <CardMedia
+                        component="img"
+                        image={`${item.img}?w=248&fit=crop&auto=format`}
+                        title={item.title}
+                        loading="lazy"
+                      />
+                      <CardContent>
+                        <Typography>
+                          {item.title}
+                        </Typography>
+                        <Typography>
+                          Description
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Dialog>
+
                   </ImageListItem>
                 ))}
               </ImageList>
