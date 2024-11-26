c6ab745a89b1cdec7675d6bcd7d06c01f15da277
gcarlab <125522911+gcarlab@users.noreply.github.com>
2024-11-26 15:32:12 -0600

    Change Dialog component to a Modal in ArtPopup.jsx. Add styling properties

--- src/ArtPopup.jsx
+++ src/ArtPopup.jsx
@@ -1,33 +1,48 @@
-import { Dialog, Typography, Stack, IconButton } from "@mui/material";
+import { Modal, Box, Typography, Stack, IconButton } from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
 
 const ArtPopup = ({open, artistById, selectedImg, handleClose}) => {
 
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
+
     return(
         <>
-            <Dialog 
+            <Modal 
             onClose={handleClose} 
             open={open}
+            keepMounted
             >
-                <Stack direction={"row"} sx={{justifyContent: "right"}}>
-                    <IconButton onClick={handleClose} sx={{padding: "0.2em"}}>
-                        <CloseIcon />
-                    </IconButton>
-                </Stack>
-                <img 
-                src={selectedImg.url}
-                alt={selectedImg.title}
-                loading="lazy"
-                style={{padding: 10}}
-                />
+                <Box sx={style}>
+                    <Stack direction={"row"} sx={{justifyContent: "right"}}>
+                        <IconButton onClick={handleClose} sx={{padding: "0.2em"}}>
+                            <CloseIcon />
+                        </IconButton>
+                    </Stack>
+                    <img 
+                    src={selectedImg.url}
+                    alt={selectedImg.title}
+                    loading="lazy"
+                    style={{padding: 10, width: "100%"}}
+                    />
 
-                <Typography loading="lazy" sx={{fontSize: 30, paddingLeft: 1}}>
-                    {selectedImg.title}
-                </Typography>
-                <Typography loading="lazy" sx={{paddingLeft: 1, paddingBottom: 1}}>
-                {"Artist: " + artistById(selectedImg.artist).name}
-                </Typography>
-            </Dialog>
+                    <Typography loading="lazy" sx={{fontSize: 30, paddingLeft: 1}}>
+                        {selectedImg.title}
+                    </Typography>
+                    <Typography loading="lazy" sx={{paddingLeft: 1, paddingBottom: 1}}>
+                    {"Artist: " + artistById(selectedImg.artist).name}
+                    </Typography>
+                </Box>
+            </Modal>
         </>
     )
 }
