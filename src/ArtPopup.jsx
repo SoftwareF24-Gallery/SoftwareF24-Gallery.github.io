75e135e99d400ab4f77078ee5b6579fa70c75440
DrakeCal <drakeacalhoun@gmail.com>
2024-11-26 16:56:42 -0600

    Huge popup change
    - Main popup is now a modal that contains the art, title, description, artist picture, artist description, and a button to show tags

--- src/ArtPopup.jsx
+++ src/ArtPopup.jsx
@@ -1,5 +1,6 @@
-import { Modal, Box, Typography, Stack, IconButton } from "@mui/material";
+import { Modal, Box, Typography, Stack, IconButton, Button, Dialog, DialogTitle} from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
+import { useState } from "react";
 
 const ArtPopup = ({open, artistById, selectedImg, handleClose}) => {
 
@@ -8,12 +9,15 @@ const ArtPopup = ({open, artistById, selectedImg, handleClose}) => {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: 400,
+        width: 600,
         bgcolor: 'background.paper',
         border: '2px solid #000',
         boxShadow: 24,
         p: 4,
       };
+    
+      //Manages tags popup opening and closing
+      const [tags, setTags] = useState(false)
 
     return(
         <>
@@ -28,19 +32,59 @@ const ArtPopup = ({open, artistById, selectedImg, handleClose}) => {
                             <CloseIcon />
                         </IconButton>
                     </Stack>
-                    <img 
-                    src={selectedImg.url}
-                    alt={selectedImg.title}
-                    loading="lazy"
-                    style={{padding: 10, width: "100%"}}
-                    />
+                    <Stack direction={"row"}>
+                        <Stack direction={"column"} sx={{justifyContent: "left", width: "150%"}}>
+                            <img 
+                                src={selectedImg.url}
+                                alt={selectedImg.title}
+                                loading="lazy"
+                                style={{padding: 10, width: "100%"}}
+                            />
+                                <Stack direction={"row"}>
+                                    <Typography loading="lazy" sx={{fontSize: 30, paddingLeft: 1}}>
+                                        {selectedImg.title}
+                                    </Typography>
+                                    <Button onClick={() => setTags(true)}>
+                                        View Tags
+                                    </Button>
 
-                    <Typography loading="lazy" sx={{fontSize: 30, paddingLeft: 1}}>
-                        {selectedImg.title}
-                    </Typography>
-                    <Typography loading="lazy" sx={{paddingLeft: 1, paddingBottom: 1}}>
-                    {"Artist: " + artistById(selectedImg.artist).name}
-                    </Typography>
+
+                                    <Dialog open={tags} onClose={() => setTags(false)} aria-labelledby="dialog-title">
+
+                                        <DialogTitle id="dialog-title" sx={{display: "flex", justifyContent: "space-between"}}>
+                                            Tags
+                                            <IconButton onClick={() => setTags(false)} sx={{padding: "0.2em"}}>
+                                                <CloseIcon />
+                                            </IconButton>
+                                        </DialogTitle>
+
+                                        <Typography loading="lazy" sx={{fontSize: 30, padding: 3}}>
+                                            {"" + selectedImg.tags}
+                                        </Typography>
+                                    </Dialog>
+
+
+                                </Stack>
+                            <Typography loading="lazy" sx={{paddingLeft: 1, paddingBottom: 1}}>
+                                {selectedImg.description}
+                            </Typography>
+                            </Stack>
+                            <Stack direction={"column"} sx={{justifyContent: "right", width: "80%"}}>
+                                <Typography loading="lazy" sx={{paddingLeft: 1, paddingBottom: 1}}>
+                                    {"By " + artistById(selectedImg.artist).name}
+                                </Typography>
+                                <img 
+                                    src={artistById(selectedImg.artist).picture}
+                                    alt={artistById(selectedImg.artist).name}
+                                    loading="lazy"
+                                    style={{padding: 10, width: "100%"}}
+                                />
+                                <Typography loading="lazy" sx={{paddingLeft: 1, paddingBottom: 1}}>
+                                    {artistById(selectedImg.artist).bio}
+                                </Typography>
+                            
+                        </Stack>
+                    </Stack>
                 </Box>
             </Modal>
         </>
