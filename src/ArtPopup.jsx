39056b32d90370784b42d7a1ab83c0912bc001dc
gcarlab <125522911+gcarlab@users.noreply.github.com>
2024-12-07 11:40:17 -0600

    Partial fixes for Art Popup sizing. Card component has been added for component control.

--- src/ArtPopup.jsx
+++ src/ArtPopup.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Modal, Box, Typography, Stack, Paper, IconButton, Button, Dialog, DialogTitle} from "@mui/material";
+import { Avatar, Modal, Card, CardMedia, CardContent, Box, Typography, Stack, Paper, IconButton, Button, Dialog, DialogTitle} from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
 import { useState } from "react";
 
@@ -11,6 +11,7 @@ const ArtPopup = ({open, artistById, selectedImg, handleClose}) => {
         transform: 'translate(-50%, -50%)',
         bgcolor: 'background.paper',
         border: '2px solid #000',
+        maxHeight: '100vh',
         boxShadow: 24,
         p: 4,
       };
@@ -25,26 +26,28 @@ const ArtPopup = ({open, artistById, selectedImg, handleClose}) => {
             open={open}
             keepMounted
             >
-                <Box sx={style}>
+                <Card sx={style}>
                     <Stack direction={"row"} sx={{justifyContent: "right"}}>
                         <IconButton onClick={handleClose}>
                             <CloseIcon />
                         </IconButton>
                     </Stack>
-                    <Stack direction={{xs: "column", md: "row"}} sx={{display: "flex", width: "inherit", height: "inherit", margin: "0 auto"}}>
+                    <Stack direction={{xs: "column"}}>
                         <Stack direction="column">
-                            <img 
-                                src={selectedImg.url}
+                            <CardMedia
+                                sx={{objectFit: "contain"}}
+                                component="img"
+                                image={selectedImg.url}
                                 alt={selectedImg.title}
-                                loading="lazy"
-                                style={{maxHeight: "100%", maxWidth: "100%", margin: "0 auto"}}
                             />
-                                <Stack direction={"row"}>
-                                    <Typography variant="h2" loading="lazy" sx={{fontSize: "2rem", lineHeight: "2.2rem"}}>
-                                        {selectedImg.title}
-                                    </Typography>
-                                    <Button variant="outlined" onClick={() => setTags(true)}>
-                                        View Tags
+                                <Stack direction={"row"} sx={{display: "flex", justifyContent: "space-between"}}>
+                                    <CardContent>
+                                        <Typography variant="h2" loading="lazy" sx={{whiteSpace: "nowrap", fontSize: "1.5rem"}}>
+                                            {selectedImg.title}
+                                        </Typography>
+                                    </CardContent>
+                                    <Button sx={{height: "2.5rem", margin: "auto 0"}} variant="outlined" onClick={() => setTags(true)}>
+                                        <Typography sx={{whiteSpace: "nowrap", fontSize: "100%", padding: "1rem"}}>View Tags</Typography>
                                     </Button>
 
                                     {/* Tags popup */}
@@ -62,15 +65,14 @@ const ArtPopup = ({open, artistById, selectedImg, handleClose}) => {
                                             {selectedImg.tags.join(", ")}
                                         </Typography>
                                     </Dialog>
-
-
                                 </Stack>
+
                             {/* Image description */}
-                            <Typography loading="lazy" sx={{}}>
+                            <Typography loading="lazy">
                                 {selectedImg.description}
                             </Typography>
                             </Stack>
-                            <Stack direction={{xs: "row", md: "column"}} sx={{}}>
+                            <Stack direction={{xs: "row"}}>
                                 {/* Artist's picture */}
                                 <Avatar 
                                     src={artistById(selectedImg.artist).picture}
@@ -78,23 +80,17 @@ const ArtPopup = ({open, artistById, selectedImg, handleClose}) => {
                                     sx={{height: "8rem", width: "8rem", margin: "1rem auto",}}
                                     loading="lazy"
                                 />
-                                <Stack direction="column">
-                                    <Typography variant="h6" sx={{padding: 1}}>
-                                        Artist
-                                    </Typography>
-                                    <Paper elevation={4} sx={{padding: 1, m: 1}}>
+                                <Paper elevation={4} sx={{padding: 1, m: 1, display: "flex", flexDirection: "column", justifyContent: "center"}}>
                                         <Typography variant="h6" loading="lazy">
                                             {artistById(selectedImg.artist).name}
                                         </Typography>
                                         <Typography loading="lazy">
                                             {artistById(selectedImg.artist).bio}
                                         </Typography>
-                                    </Paper>
-                                </Stack>
-                            
+                                </Paper>
                         </Stack>
                     </Stack>
-                </Box>
+                </Card>
             </Modal>
         </>
     )
